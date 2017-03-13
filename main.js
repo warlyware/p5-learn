7f59a14a81231ddd2018d1e203d864ecc7b2b3b1
Dan Ward <daniel.ward@forrent.com>
2017-03-13 15:09:54 -0400

    Vary speed of incoming dots

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var counter = 0;
+
 var pt = {
     x: 300,
     y: 50
@@ -15,15 +17,17 @@ function setup() {
 }
 
 function draw() {
-    col.r = random(100, 255);
-    col.g = 0;
-    col.b = random(100, 200);
-
-    pt.x = random(0, width);
-    pt.y = random(0, width);
-    fill(col.r, col.g, col.b, 100);
-    noStroke();
-    ellipse(pt.x, pt.y, 24, 24);
+    if (Number.isInteger(counter/2)) {
+        col.r = random(100, 255);
+        col.g = 0;
+        col.b = random(100, 200);
+        pt.x = random(0, width);
+        pt.y = random(0, height);
+        fill(col.r, col.g, col.b, 100);
+        noStroke();
+        ellipse(pt.x, pt.y, 24, 24);
+    }
+    counter += 1;
 }
 
 window.onresize = function() {
