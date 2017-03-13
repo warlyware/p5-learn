9cc27628e7630748b103acc60d2cf5a3358a5b8c
Dan Ward <daniel.ward@forrent.com>
2017-03-13 11:49:07 -0400

    Use object for circle instead of loose vars

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var x = 0;
-var y = 200;
-var diameter = 50;
+
 var r = 218;
 var g = 160;
 var b = 221;
+
+var circle = {
+    x: 0,
+    y: 200,
+    diameter: 50
+}
 
 function setup() {
     createCanvas(640, 360);
@@ -12,7 +16,7 @@ function setup() {
 function draw() {
     background(r, g, b);
     fill(250, 200, 200);
-    ellipse(x, y, diameter, diameter);
+    ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
 
-    x = x + 1;
+    circle.x = circle.x + 1;
 }
