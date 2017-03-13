d7e8d2e4dcb86eccdf667fd99363d5be7af2abf5
Dan Ward <daniel.ward@forrent.com>
2017-03-13 12:05:58 -0400

    Use map to recolor canvas background

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-
-var r = 218;
-var g = 160;
-var b = 221;
+var r = 0;
+var b = 255;
 
 var circle = {
-    x: 0,
     y: 200,
     diameter: 50
 }
@@ -14,9 +11,11 @@ function setup() {
 }
 
 function draw() {
-    background(r, g, b);
+    r = map(mouseX, 0, 600, 0, 255);
+    b = map(mouseX, 0, 600, 255, 0);
+    background(r, 0, b);
     fill(250, 200, 200);
-    ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
+    ellipse(mouseX, circle.y, circle.diameter, circle.diameter);
 
     circle.x = circle.x + 1;
 }
