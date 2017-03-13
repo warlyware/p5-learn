87208932de679856874e525f0f0584a1e76c276b
Dan Ward <daniel.ward@forrent.com>
2017-03-13 11:43:18 -0400

    Use simple maths and vars to move circle across canvas

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+var x = 0;
+var y = 200;
+var diameter = 50;
+var r = 218;
+var g = 160;
+var b = 221;
+
 function setup() {
     createCanvas(640, 360);
-    print('setup function');
 }
 
 function draw() {
-    background(0, 0, 255);
-    rect(100, 200, 75, 125);
+    background(r, g, b);
+    fill(250, 200, 200);
+    ellipse(x, y, diameter, diameter);
+
+    x = x + 1;
 }
