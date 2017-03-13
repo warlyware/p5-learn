5655551b42a07e486a8865ab07b39257c55e99ae
Dan Ward <daniel.ward@forrent.com>
2017-03-13 15:15:21 -0400

    Use condition to fill circle

--- main.js
+++ main.js
@@ -1,36 +1,21 @@
-var counter = 0;
-
-var pt = {
-    x: 300,
-    y: 50
-}
-
-var col = {
-    r: 255,
-    g: 0,
-    b: 0
-}
-
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    background(0);
 }
 
 function draw() {
-    if (Number.isInteger(counter/2)) {
-        col.r = random(100, 255);
-        col.g = 0;
-        col.b = random(100, 200);
-        pt.x = random(0, width);
-        pt.y = random(0, height);
-        fill(col.r, col.g, col.b, 100);
-        noStroke();
-        ellipse(pt.x, pt.y, 24, 24);
+    background(0);
+
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+
+    if (mouseX > window.innerWidth / 2) {
+        fill(255, 0, 200);
     }
-    counter += 1;
+
+    ellipse(window.innerWidth / 2, window.innerHeight / 2, 100, 100);
 }
 
 window.onresize = function() {
     createCanvas(window.innerWidth, window.innerHeight);
-    background(0);
 };
